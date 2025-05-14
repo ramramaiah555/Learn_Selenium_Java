50a37f66ed8c8b947562d0f5df8f64a68a4ccf61
ramramaiah555 <ramkumar.ramaiah@qeagle.com>
2025-05-14 17:34:29 +0530

    Adding Edge Driver in Base class

--- src/main/java/week5/day1/BaseClass.java
+++ src/main/java/week5/day1/BaseClass.java
@@ -5,6 +5,7 @@ import java.time.Duration;
 import org.apache.poi.util.SystemOutLogger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -12,13 +13,15 @@ import org.testng.annotations.BeforeTest;
 
 public class BaseClass {
 	
-	public static ChromeDriver driver;
+//	public static ChromeDriver driver;
+	public static EdgeDriver driver;
 	public static String leadID;
 	
 	@BeforeTest
 	public void preCondition() {
 		
-		driver  = new ChromeDriver();
+//		driver  = new ChromeDriver();
+		driver  = new EdgeDriver();
 		driver.manage().window().maximize();
 		driver.get("http://leaftaps.com/opentaps/control/main");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
@@ -26,8 +29,7 @@ public class BaseClass {
 		driver.findElement(By.id("password")).sendKeys("crmsfa");
 		driver.findElement(By.className("decorativeSubmit")).click();
 		driver.findElement(By.linkText("CRM/SFA")).click();
-		
-		
+			
 	}
 	
 	@AfterTest
